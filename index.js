2808772de88d5d37c0dbb888b65f4c32eae6e474
Fehp <fernandop09@hotmail.com>
2021-05-23 20:58:48 -0300

    Fix fetch from api

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ let result = {
   productList: [],
 };
 
-let archive = "codebeautify.json";
-fetch(archive)
+let url =
+  "https://www.armazempb.com.br/ccstoreui/v1/products?totalResults=true&totalExpandedResults=true&catalogId=cloudCatalog&limit=60&offset=0&categoryId=eletrodomestico&includeChildren=true&storePriceListGroupId=reais";
+
+fetch(url)
   .then(async (response) => response.json())
   .then(async (data) => {
     result.category = data.category.displayName;
